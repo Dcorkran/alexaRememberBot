119494b49ba5154db2a81d95304b27a7d5c729bd
Dillon Corkran <dcorkran972@gmail.com>
2017-04-17 16:22:35 -0600

    :+1: restructure

--- index.js
+++ index.js
@@ -2,26 +2,16 @@
 var Alexa = require('alexa-sdk');
 var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
-var languageStrings = {
-    "en": {
-        "translation": {
-            "SKILL_NAME" : "Remember Bot",
-            "GET_NAME_MESSAGE" : "Here's what I remember: ",
-            "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
-            "HELP_REPROMPT" : "What can I help you with?",
-            "STOP_MESSAGE" : "Goodbye!"
-        }
-    }
-};
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
+
+var RESPONSE_TEST = 'Hello!!!';
+var GET_NAME_MESSAGE = "Here's what I remember: ";
 
 var handlers = {
     'LaunchRequest': function () {
@@ -38,8 +28,9 @@ var handlers = {
         // var randomFact = factArr[factIndex];
 
         // Create speech output
-        var speechOutput = this.t("GET_NAME_MESSAGE") + 'name test';
-        this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), 'test success')
+        // var speechOutput = GET_NAME_MESSAGE + 'name test';
+        this.emit(':tell', RESPONSE_TEST)
+        // this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), 'test success')
     },
     'AMAZON.HelpIntent': function () {
         var speechOutput = this.t("HELP_MESSAGE");
